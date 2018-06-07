6bbfac1c333cbcabc7c4651620e1f4efeccef348
André Silva <1141558@isep.ipp.pt>
2018-06-07 22:42:50 +0100

    fix warning [UC-03] [Implementation] [resolve bug #9]

--- src/main/java/lapr/project/model/EventRegister.java
+++ src/main/java/lapr/project/model/EventRegister.java
@@ -17,6 +17,7 @@ import java.util.List;
  */
 public class EventRegister implements Serializable{
     
+    private static final long serialVersionUID = 1L;
     
     private List<Event> eventList;
 
