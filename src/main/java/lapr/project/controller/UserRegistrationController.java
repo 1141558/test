3093f0b1ec25260381e202264d3ee800cfd913a4
José Magalhães <1171281@isep.ipp.pt>
2018-06-07 21:43:30 +0100

    Modifies UserRegistrationController to accept String password [UC-04] [Implementation] [refs issue #12]

--- src/main/java/lapr/project/controller/UserRegistrationController.java
+++ src/main/java/lapr/project/controller/UserRegistrationController.java
@@ -5,6 +5,7 @@
  */
 package lapr.project.controller;
 
+import java.util.Arrays;
 import lapr.project.model.User;
 
 /**
@@ -13,7 +14,7 @@ import lapr.project.model.User;
  */
 public class UserRegistrationController {
     
-    private User user;
+    private User user=new User();
     
     public UserRegistrationController(){
         
@@ -22,11 +23,66 @@ public class UserRegistrationController {
     
     
     
-    public boolean setData(String name, String email, String username, int password){
+    public boolean setData(String name, String email, String username, String password){
+        
+        user.setName(name);
+        user.setEmail(email);
+        user.setUsername(username);
+        
+        System.out.println(encryptPassword(password));
+        user.setPassword(encryptPassword(password));
+        
      
         
         return true;
     }
+    
+    
+    
+    
+    private double encryptPassword(String password){
+        double encrypted=0.0;
+        
+        int [] numCount=new int [10];
+        Arrays.fill(numCount,0);
+        
+        //conta as repetições de cada algarismo
+        for(int i=0;i<password.length();i++){
+            char c=password.charAt(i);
+            int digit=Character.getNumericValue(c);
+            numCount[digit]++;
+            
+        }
+        
+        //Calcula as probabilidades
+        double [] probArray=new double [10];
+        for(int i=0;i<probArray.length;i++){
+            probArray[i]=(double)((double)numCount[i]/password.length());
+        }
+        
+        
+        double I=0.0, F=1.0;
+        double x=0.0,y=1.0;
+        
+        //Codifica
+        for(int i=0;i<password.length();i++){
+            char c=password.charAt(i);
+            int digit=Character.getNumericValue(c);
+            
+            //I+delta*probC(digit-1)
+            
+            x=(digit==0)? (double)I:(double)I+(((double)F-(double)I)*probArray[digit-1]);
+            y=(double)I+(((double)F-(double)I)*probArray[digit]);
+            
+            I=(double)x;
+            F=(double)y;
+            
+        }
+        encrypted=(double)I;
+        return encrypted;
+    }
+    
+    
     
     
     
