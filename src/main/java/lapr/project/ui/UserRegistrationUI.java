93f86ff406ca01372e0f6aece41686c27902d7bb
José Magalhães <1171281@isep.ipp.pt>
2018-06-08 11:47:32 +0100

    Implement user registration in exhibition center (Class UserRegistrationController) [UC-04] [Implementation] [refs issue #12]

--- src/main/java/lapr/project/ui/UserRegistrationUI.java
+++ src/main/java/lapr/project/ui/UserRegistrationUI.java
@@ -25,10 +25,10 @@ public class UserRegistrationUI {
     private UserRegistrationController controller;
 
     public UserRegistrationUI(ExhibitionCentre exhibitionCentre){
-        this.controller= new UserRegistrationController();
+        this.controller= new UserRegistrationController(exhibitionCentre);
         
         String name="", email="", username="", passwordString="";
-        int password=0;
+//        int password=0;
         
         //Request user data
         
@@ -77,7 +77,9 @@ public class UserRegistrationUI {
         
 //        password=Integer.parseInt(passwordString);
        
-        if (controller.setData(name, email, username, passwordString)) {
+        controller.setData(name, email, username, passwordString);
+        
+        if (controller.addUser()) {
             //confirma sucesso
             Utils.printConfirmation("User registered!");
         } else {
