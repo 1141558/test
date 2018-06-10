34d60789624ab8dcb54b79b472dff6e16bbc0ebe
José Magalhães <1171281@isep.ipp.pt>
2018-06-10 21:50:19 +0100

    Create comments (UserRegistrationController) [UC-04] [Implementation] [refs issue #12]

--- src/main/java/lapr/project/controller/UserRegistrationController.java
+++ src/main/java/lapr/project/controller/UserRegistrationController.java
@@ -5,7 +5,6 @@
  */
 package lapr.project.controller;
 
-import java.util.Arrays;
 import lapr.project.model.ExhibitionCentre;
 import lapr.project.model.User;
 import static lapr.project.model.PasswordEncryption.encryptPassword;
@@ -15,38 +14,51 @@ import static lapr.project.model.PasswordEncryption.encryptPassword;
  * @author JM
  */
 public class UserRegistrationController {
-    
+
     private ExhibitionCentre exhibitionCentre;
     private User user;
-    
-    public UserRegistrationController(ExhibitionCentre exhibitionCentre){
-        this.exhibitionCentre=exhibitionCentre;
-        this.user=new User();
+
+    /**
+     * Constructor
+     *
+     * @param exhibitionCentre Exhibition centre in use, introduced as parameter
+     */
+    public UserRegistrationController(ExhibitionCentre exhibitionCentre) {
+        this.exhibitionCentre = exhibitionCentre;
+        this.user = new User();
     }
-    
-    
-    
-    
-    public void setData(String name, String email, String username, String password){
-        
+
+    /**
+     * Method to store/modify data
+     *
+     * @param name Name to be stored in the User
+     * @param email Email to be stored in the User
+     * @param username Username to be stored in the User
+     * @param password Password to be stored in the User
+     */
+    public void setData(String name, String email, String username, String password) {
         this.user.setName(name);
         this.user.setEmail(email);
         this.user.setUsername(username);
-        
-        System.out.println(encryptPassword(password));
         this.user.setPassword(encryptPassword(password));
-        
-     
-        
     }
-    
-    
+
+    /**
+     * Method to add the current user to the User list in the exhibition centre
+     *
+     * @return a boolean value defining if the user was successfully added to
+     * the register
+     */
     public boolean addUser() {
         return this.exhibitionCentre.getUserRegister().addUser(this.user);
     }
-    
-    public User getUser(){
+
+    /**
+     * Method to return the current user
+     *
+     * @return The user stored
+     */
+    public User getUser() {
         return user;
     }
-    
 }
