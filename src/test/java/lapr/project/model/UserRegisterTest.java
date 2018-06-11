da2357ebddfa84f9aecd2e545154eedb82aaf135
José Magalhães <1171281@isep.ipp.pt>
2018-06-11 02:38:18 +0100

    Creation of test class (UserRegisterTest) [UC-04] [Implementation] [refs issue #12]

---
+++ src/test/java/lapr/project/model/UserRegisterTest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JM
+ */
+public class UserRegisterTest {
+    
+    /**
+     * Test of getUser method, of class UserRegister.
+     */
+    @Test
+    public void testGetUser() {
+//        System.out.println("getUser");
+//        UserRegister instance = new UserRegister();
+//        User expResult = null;
+//        User result = instance.getUser();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setUser method, of class UserRegister.
+     */
+    @Test
+    public void testSetUser() {
+//        System.out.println("setUser");
+//        User user = null;
+//        UserRegister instance = new UserRegister();
+//        instance.setUser(user);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUserList method, of class UserRegister.
+     */
+    @Test
+    public void testGetUserList() {
+//        System.out.println("getUserList");
+//        UserRegister instance = new UserRegister();
+//        List<User> expResult = null;
+//        List<User> result = instance.getUserList();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setUserList method, of class UserRegister.
+     */
+    @Test
+    public void testSetUserList() {
+//        System.out.println("setUserList");
+//        List<User> userList = null;
+//        UserRegister instance = new UserRegister();
+//        instance.setUserList(userList);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addUser method, of class UserRegister.
+     */
+    @Test
+    public void testAddUser() {
+//        System.out.println("addUser");
+//        User user = null;
+//        UserRegister instance = new UserRegister();
+//        boolean expResult = false;
+//        boolean result = instance.addUser(user);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of userExists method, of class UserRegister.
+     */
+    @Test
+    public void testUserExists() {
+//        System.out.println("userExists");
+//        User user = null;
+//        UserRegister instance = new UserRegister();
+//        boolean expResult = false;
+//        boolean result = instance.userExists(user);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
