2ac35114d71358e62176c9816c1702a50290d70e
José Magalhães <1171281@isep.ipp.pt>
2018-06-11 01:26:54 +0100

    Create tests for Password Encryption (PasswordEncryptionTest) [UC-04] [Implementation] [refs issue #12]

---
+++ src/test/java/lapr/project/model/PasswordEncryptionTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author JM
+ */
+public class PasswordEncryptionTest {
+
+    /**
+     * Test of encryptPassword method, of class PasswordEncryption.
+     */
+    @Test
+    public void testEncryptPassword() {
+        System.out.println("encryptPassword");
+        String password = "1321";
+        double expResult = 0.4375;
+        System.out.println(PasswordEncryption.encryptPassword(password));
+        double result = PasswordEncryption.encryptPassword(password);
+        assertEquals(expResult, result, 0.0);
+    }
+}
