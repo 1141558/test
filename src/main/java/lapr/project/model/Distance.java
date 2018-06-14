d9ded9aa047dde1355a96219a7a8b8f0bb0ea096
andre <1141558@isep.ipp.pt>
2018-06-14 21:13:44 +0100

    refactor Distance [UC-10][Implementation][ref #27]

--- src/main/java/lapr/project/model/Distance.java
+++ src/main/java/lapr/project/model/Distance.java
@@ -5,19 +5,24 @@
  */
 package lapr.project.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author MariaJoão
  */
-public class Distance {
+public class Distance implements Comparable<Distance>, Serializable{
+    
+    private static final long serialVersionUID = 1L;
     
     private String description;
-    private double value;
+    private Double value;
 
     public Distance() {
     }
 
-    public Distance(String description, double value) {
+    public Distance(String description, Double value) {
         this.description = description;
         this.value = value;
     }
@@ -39,16 +44,55 @@ public class Distance {
     /**
      * @return the value
      */
-    public double getValue() {
+    public Double getValue() {
         return value;
     }
 
     /**
      * @param value the value to set
      */
-    public void setValue(double value) {
+    public void setValue(Double value) {
         this.value = value;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.description);
+        hash = 61 * hash + Objects.hashCode(this.value);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Distance other = (Distance) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.value, other.value);
+    }
     
+    
+
+    
+    @Override
+    public String toString() {
+        return "description= " + description + ", value= " + value;
+    }    
+
+    @Override
+    public int compareTo(Distance o) {
+        int cmp = value > o.value ? +1 : value < o.value ? -1 : 0;
+        return cmp;   
+    }
     
 }
