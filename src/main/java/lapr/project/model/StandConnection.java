58b09a3df28653c224bdde71024ee9528578faf5
José Magalhães <1171281@isep.ipp.pt>
2018-06-15 16:17:02 +0100

    Create StandConnection model Class (StandConnection) [UC-34] [Implementation] [refs issue #72]

---
+++ src/main/java/lapr/project/model/StandConnection.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+/**
+ *
+ * @author JM
+ */
+public class StandConnection implements Comparable <StandConnection> {
+    String a;
+    String b;
+    double dist;
+    
+    public StandConnection(String a, String b, double dist){
+        this.a=a;
+        this.b=b;
+        this.dist=dist;
+    }
+    
+    
+    public String getA(){
+        return this.a;
+    }
+    
+    public String getB(){
+        return this.b;
+    }
+    
+    public double getDist(){
+        return this.dist;
+    }
+    
+
+
+    @Override
+    public int compareTo(StandConnection o) {
+        if(this.dist<o.getDist()){
+            return -1;
+        }
+        if(this.dist>o.getDist()){
+            return 1;
+        }
+        return 0;
+    }
+    
+    
+    @Override
+    public String toString(){
+        return a+" --> "+b+"  ("+dist+" meters)";
+    }
+}
+
+
+       
