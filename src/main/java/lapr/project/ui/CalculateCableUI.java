b45430c4702e543c9be9731a061c3438e6e9fb0c
José Magalhães <1171281@isep.ipp.pt>
2018-06-18 17:50:54 +0100

    Added verifications (CalculateCableUI) [UC-34] [Implementation] [refs issue #72]

--- src/main/java/lapr/project/ui/CalculateCableUI.java
+++ src/main/java/lapr/project/ui/CalculateCableUI.java
@@ -7,30 +7,24 @@ package lapr.project.ui;
 
 import java.util.ArrayList;
 import java.util.List;
-import lapr.project.controller.CalculateCableController;
-import lapr.project.controller.UserRegistrationController;
 import lapr.project.model.Event;
 import lapr.project.model.ExhibitionCentre;
 import lapr.project.utils.Utils;
-import static lapr.project.controller.CalculateCableController.*;
-import lapr.project.model.CalculateElectricalCable;
 import lapr.project.model.StandConnection;
+import lapr.project.controller.CalculateCableController;
 
 /**
  *
  * @author JM
  */
 public class CalculateCableUI {
-    
-    
-    
+
     private CalculateCableController controller;
 
-    
-    public CalculateCableUI(ExhibitionCentre exhibitionCentre){
+    public CalculateCableUI(ExhibitionCentre exhibitionCentre) {
         this.controller = new CalculateCableController(exhibitionCentre);
-        String selection="";
-        
+        String selection = "";
+
         //Request selection of event
         System.out.println("");
         System.out.println((char) 27 + "[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + (char) 27 + "[0m");
@@ -39,16 +33,16 @@ public class CalculateCableUI {
 
         //Apresentar eventos
         System.out.println("EVENTS:");
-        List<Event> events=controller.getEventList();
-        for(Event ev:events){
+        List<Event> events = controller.getEventList();
+        for (Event ev : events) {
             System.out.println(ev.getTitle());
         }
-        
+
         //Selecctionar evento
+        System.out.println("SELECT EVENT ('Q' TO QUIT)");
         selection = Utils.readLineFromConsole("EVENT TITLE: ");
-        
-      //        while (!exhibitionCentre.getEventRegister().getEventList().iterator().toString().contains(selection)){
-        while (!controller.eventExists(selection)) {
+
+        while (!controller.eventExists(selection) && (!selection.equalsIgnoreCase("Q"))) {
             Utils.printError("EVENT NOT FOUND!\nPLEASE TRY AGAIN");
             System.out.println("EVENTS:");
             for (Event ev : events) {
@@ -56,25 +50,30 @@ public class CalculateCableUI {
             }
 
             //Seleccionar evento
+            System.out.println("SELECT EVENT ('Q' TO QUIT)");
             selection = Utils.readLineFromConsole("EVENT TITLE: ");
         }
         
-        
-        //Evento seleccionado e encontrado
-        //Calcular
-        ArrayList<StandConnection> tree = new ArrayList<>(controller.calcPath(selection));
-        double length = controller.calcLength(selection);
-        
-        for(StandConnection connect : tree){
-            System.out.println(connect.toString());
+        if(!controller.hasStands(controller.findEvent(selection))){
+            Utils.printError("THE EVENT SELECTED HAS NO STANDS!");
+            new InitialMenuUI(exhibitionCentre);
         }
-        System.out.println("Total: " + length + " meters");
-        
-        
-        
+
+        if ((!selection.equalsIgnoreCase("Q"))&&(controller.hasStands(controller.findEvent(selection)))) {
+
+            //Evento seleccionado e encontrado
+            //Calcular
+            ArrayList<StandConnection> tree = new ArrayList<>(controller.calcPath(selection));
+            double length = controller.calcLength(selection);
+
+            for (StandConnection connect : tree) {
+                System.out.println(connect.toString());
+            }
+            System.out.println("Total: " + length + " meters");
+        }
+
         new InitialMenuUI(exhibitionCentre);
-        
+
     }
-    
-    
+
 }
