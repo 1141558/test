e00677b67480e0b97efab1e00b5b05644b140a3e
José Magalhães <1171281@isep.ipp.pt>
2018-06-19 16:19:11 +0100

    Revision and creation of tests (UserRegistrationControllerTest) [UC-04] [Implementation] [refs issue #12]

--- src/test/java/lapr/project/controller/UserRegistrationControllerTest.java
+++ src/test/java/lapr/project/controller/UserRegistrationControllerTest.java
@@ -16,14 +16,14 @@ import org.junit.jupiter.api.Test;
  * @author JM
  */
 public class UserRegistrationControllerTest {
-    
+
     /**
      * Test of setData method, of class UserRegistrationController.
      */
     @Test
     public void testSetData() {
         System.out.println("setData");
-        User user=new User("nome completo","email@server.com","user",PasswordEncryption.encryptPassword("12345678"));
+        User user = new User("nome completo", "email@server.com", "user", PasswordEncryption.encryptPassword("12345678"));
         String name = "nome completo";
         String email = "email@server.com";
         String username = "user";
@@ -34,8 +34,8 @@ public class UserRegistrationControllerTest {
         assertEquals(user.getName(), instance.getUser().getName());
         assertEquals(user.getEmail(), instance.getUser().getEmail());
         assertEquals(user.getUsername(), instance.getUser().getUsername());
-        assertEquals(user.getPassword(), instance.getUser().getPassword(),0.0);
-        
+        assertEquals(user.getPassword(), instance.getUser().getPassword(), 0.0);
+
     }
 
     /**
@@ -53,7 +53,90 @@ public class UserRegistrationControllerTest {
         instance.setData(name, email, username, password);
         instance.addUser();
         assertTrue(centre.getUserRegister().getUserList().contains(instance.getUser()));
-        
+
     }
-    
+
+    /**
+     * Test of isName method, of class UserRegistrationUI.
+     */
+    @Test
+    public void testIsName() {
+        System.out.println("isName");
+        String name = "Nome Nome";
+        String notName = "123 José";
+        ExhibitionCentre centre = new ExhibitionCentre();
+        UserRegistrationController instance = new UserRegistrationController(centre);
+        boolean expResult = true;
+        boolean result = instance.isName(name);
+        boolean result2 = instance.isName(notName);
+        assertEquals(expResult, result);
+        assertNotEquals(expResult, result2);
+    }
+
+    /**
+     * Test of isEmail method, of class UserRegistrationUI.
+     */
+    @Test
+    public void testIsEmail() {
+        System.out.println("isEmail");
+        String email = "emailtest@gmail.com";
+        String notEmail = "emailtest@gmail.com@hotmail.com";
+        ExhibitionCentre centre = new ExhibitionCentre();
+        UserRegistrationController instance = new UserRegistrationController(centre);
+        boolean expResult = true;
+        boolean result = instance.isEmail(email);
+        boolean result2 = instance.isEmail(notEmail);
+        assertEquals(expResult, result);
+        assertNotEquals(expResult, result2);
+    }
+
+    /**
+     * Test of isUsername method, of class UserRegistrationUI.
+     */
+    @Test
+    public void testIsUsername() {
+        System.out.println("isUsername");
+        String username = "user123";
+        String notUsername = "";
+        ExhibitionCentre centre = new ExhibitionCentre();
+        UserRegistrationController instance = new UserRegistrationController(centre);
+        boolean expResult = true;
+        boolean result = instance.isUsername(username);
+        boolean result2 = instance.isUsername(notUsername);
+        assertEquals(expResult, result);
+        assertNotEquals(expResult, result2);
+    }
+
+    /**
+     * Test of isPassword method, of class UserRegistrationUI.
+     */
+    @Test
+    public void testIsPassword() {
+        System.out.println("isPassword");
+        String passString = "12345678";
+        String notPassString = "12345abc";
+        ExhibitionCentre centre = new ExhibitionCentre();
+        UserRegistrationController instance = new UserRegistrationController(centre);
+        boolean expResult = true;
+        boolean result = instance.isPassword(passString);
+        boolean result2 = instance.isPassword(notPassString);
+        assertEquals(expResult, result);
+        assertNotEquals(expResult, result2);
+    }
+
+    /**
+     * Test of getUser method, of class UserRegistrationController.
+     */
+    @org.junit.Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        User user = new User("nome completo", "email@server.com", "user", PasswordEncryption.encryptPassword("12345678"));
+        ExhibitionCentre centre = new ExhibitionCentre();
+        UserRegistrationController instance = new UserRegistrationController(centre);
+        instance.setData("nome completo", "email@server.com", "user", "12345678");
+        User expResult = user;
+        User result = instance.getUser();
+        assertEquals(expResult, result);
+    }
+
 }
